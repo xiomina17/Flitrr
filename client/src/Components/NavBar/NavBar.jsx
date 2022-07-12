1ad1eceb480a2d05f483342f4bd01d7d9a492e60
StaticCloud <diegomarrs@gmail.com>
2022-07-11 19:57:03 -0700

    dropdown navbar

--- client/src/Components/NavBar/NavBar.jsx
+++ client/src/Components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import './Navbar.css'
-import {Button, Container, Nav, Navbar as BNavbar} from 'react-bootstrap';
+import {Button, Container, Nav, Navbar as BNavbar, NavDropdown} from 'react-bootstrap';
 
 
 const Navbar = () => {
@@ -11,14 +11,38 @@ const Navbar = () => {
       <Container>
       <div className='loguito'>Flitrr</div>
       <BNavbar.Toggle aria-controls="responsive-navbar-nav" />
-      <BNavbar.Collapse id="responsive-navbar-nav">
-        <Nav className='me-auto'></Nav>
-        <Nav>
-          <Link to="/product"><p>Product</p></Link>
-          <Link to="/signup"><Button size="lg" className='nav-button'>Get Started</Button></Link>
-          <Link to="/login"><Button size="lg" className='nav-button'>Log in</Button></Link>
-        </Nav>
-      </BNavbar.Collapse>
+        <BNavbar.Collapse id="responsive-navbar-nav">
+          <Nav className='me-auto' style={{paddingLeft: '16px'}}>
+            <NavDropdown className="item" title="Product">
+
+            </NavDropdown>
+            <Link to="/product">
+              
+            </Link>
+            <NavDropdown className="item" title="Industries">
+
+            </NavDropdown>
+            <Link to="/product" className="item">
+              
+            </Link>
+            <NavDropdown className="item" title="Resources">
+
+            </NavDropdown>
+            <Link to="/product" className="item">
+              
+            </Link>
+            <NavDropdown className="item" title="Company">
+
+            </NavDropdown>
+            <Link to="/product" className="item">
+              
+            </Link>
+          </Nav>
+          <Nav>
+            <Link to="/signup"><Button size="lg" className='nav-button'>Get Started</Button></Link>
+            <Link to="/login"><Button size="lg" className='nav-button'>Log in</Button></Link>
+          </Nav>
+        </BNavbar.Collapse>
       </Container>
     </BNavbar>
   )
