06f0dee0d0202c8005d2115484522833389c2537
StaticCloud <diegomarrs@gmail.com>
2022-07-13 13:15:48 -0700

    added Flitrr vs... sections and argument section

--- client/src/Components/LandingPage/LandingPage.jsx
+++ client/src/Components/LandingPage/LandingPage.jsx
@@ -4,6 +4,9 @@ import Navbar from '../NavBar/NavBar'
 import MainLandingView from '../MainLandingView/MainLandingView';
 import SecondLandingView from '../SecondLandingView/SecondLandingView';
 import Pricing from '../Princing/Pricing';
+import FlitrrVCSS from '../FlitrrVCSS/FlitrrVCSS';
+import FlitrrVCRM from '../FlitrrVCRM/FlitrrVCRM';
+import Argument from '../Argument/Argument'
 import DemoComponent from '../DemoComponent/DemoComponent';
 import Footer from '../Footer/Footer';
 
@@ -21,6 +24,9 @@ const LandingPage = () => {
       <Navbar/>
       <MainLandingView/>
       <SecondLandingView/>
+      <Argument/>
+      <FlitrrVCSS/>
+      <FlitrrVCRM/>
       <Pricing/>
       <DemoComponent/>
       <Footer/>
