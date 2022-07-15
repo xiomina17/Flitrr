c508477b0dde505e65ed9323fc9966bd3c673604
StaticCloud <diegomarrs@gmail.com>
2022-07-15 16:40:00 -0700

    updated footer section

--- client/src/Components/DemoComponent/DemoComponent.jsx
+++ client/src/Components/DemoComponent/DemoComponent.jsx
@@ -1,29 +1,31 @@
 import './demoComponent.css'
-import {Button} from 'react-bootstrap'
+import '../section.css'
+import { Button, Container, Row, Col } from 'react-bootstrap'
 
 import React from 'react'
 
 const DemoComponent = () => {
   return (
-   <>
-   
-   <div className='demo-wrapper'>
-
-    <div className='rectangle'>
-        <h1 id="tittle">Try out Flitrr!</h1>
-
-        <Button href="https://calendly.com/jacob_flitrr/scheduled?month=2022-07" className="rec-button" size="lg">Schedule Demo</Button>
-
+    <div className="section-demo">
+      <Container>
+        <Row>
+          <Col md="12">
+            <div id="demo-content-wrap">
+              <h3>Try out Flitrr!</h3>
+              <Button href="https://calendly.com/jacob_flitrr/scheduled?month=2022-07" className="rec-button" size="lg">Schedule Demo</Button>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     </div>
-
-
-   </div>
-   
-   
-   
-   
-   
-   </>
+  //  <>  
+  //  <div className='demo-wrapper'>
+  //   <div className='rectangle'>
+  //       <h1 id="tittle">Try out Flitrr!</h1>
+  //       <Button href="https://calendly.com/jacob_flitrr/scheduled?month=2022-07" className="rec-button" size="lg">Schedule Demo</Button>
+  //   </div>
+  //  </div>
+  //  </>
   )
 }
 
